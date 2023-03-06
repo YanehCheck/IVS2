0682f406fcbba78320c6a16aa22de96deb2ebbc2
Michael Janeček <michael.janecek.159@seznam.cz>
2023-03-06 12:31:01 +0100

    Added Parse() method to parser and it's return type.
    Return type = ParseResult

---
+++ src/IvsProject/MathParser/ParseResult.cs
@@ -0,0 +1,39 @@
+﻿namespace MathParser 
+{
+    /// <summary>
+    /// The result of parsing by <see cref="Parser"/> class.
+    /// Holds the resulting expression (<see cref="Expression"/>) and if syntax error (<see cref="SyntaxError"/>) occurred.
+    /// </summary>
+    public record ParseResult 
+    {
+        /// <summary>
+        /// Parsed expression in reverse polish notation.
+        /// </summary>
+        public List<string> Expression { get; init; }
+        /// <summary>
+        /// Indicates if there was an error in the input expression.
+        /// </summary>
+        public bool SyntaxError { get; init; }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ParseResult"/>.
+        /// </summary>
+        /// <param name="expression"> Indicates if there was an error in the input expression. </param>
+        /// <param name="syntaxError"> Parsed expression in reverse polish notation. </param>
+        public ParseResult(List<string> expression, bool syntaxError) 
+        {
+            Expression = expression;
+            SyntaxError = syntaxError;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ParseResult"/> record with an empty <see cref="Expression"/>.
+        /// </summary>
+        /// <param name="syntaxError"> Indicates if there was an error in the input expression. </param>
+        public ParseResult(bool syntaxError) 
+        {
+            Expression = new List<string>();
+            SyntaxError = syntaxError;
+        }
+    }
+}
--- src/IvsProject/MathParser/Parser.cs
+++ src/IvsProject/MathParser/Parser.cs
@@ -2,6 +2,19 @@
 {
     public class Parser 
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="Parser"/> class.
+        /// </summary>
+        public Parser() { }
 
+        /// <summary>
+        /// Converts specified mathematical expression to reverse polish form.
+        /// </summary>
+        /// <param name="expression"> Mathematical expression. </param>
+        /// <returns> <see cref="ParseResult"/> </returns>
+        public ParseResult Parse(string expression) 
+        {
+            throw new NotImplementedException();
+        }
     }
 }
