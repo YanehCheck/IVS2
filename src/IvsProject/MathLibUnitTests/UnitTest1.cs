e8c59f39123d2b4b50974135005ca40bf39fa9bb
YanehCheck <101875519+YanehCheck@users.noreply.github.com>
2023-03-07 22:58:23 +0100

    Added simple test structure

--- src/IvsProject/MathLibUnitTests/UnitTest1.cs
+++ src/IvsProject/MathLibUnitTests/UnitTest1.cs
@@ -2,8 +2,68 @@ namespace MathLibUnitTests
 {
     public class UnitTest1 
     {
-        [Fact]
-        public void Test1() 
+        /* Added simple test structure
+         * ------------------------------------------------------------------------
+         * Do not forget to test the exceptions!
+         * i.e. negative int in factorial, divide by zero (see documentation for the full list of possible exceptions),
+         * but try to find unspecified exceptions :)
+         * To match house style prototype for testing exceptions should for example look like this:
+         * public void DivideTest_ExpectDivideByZeroException<T>(T leftOperand, T rightOperand, T expectedResult)
+         *
+         * Do not forget to test multiple types!
+         * Not only integers - i.e.              [InlineData(5,4,3)]
+         * But also decimals - i.e.              [InlineData(5m,4m,3m)]
+         * and other types like double, float, and so on...
+         * Also do not forget about edge cases like NaN, Infinity and more.
+         * (see https://learn.microsoft.com/en-us/dotnet/standard/numerics)
+         * ------------------------------------------------------------------------
+         * Also do not forget to delete this :P
+         */
+
+        //[Theory]
+        public void AddTest<T>(T leftOperand, T rightOperand, T expectedResult) 
+        {
+
+        }
+
+        //[Theory]
+        public void SubtractTest<T>(T leftOperand, T rightOperand, T expectedResult) 
+        {
+
+        }
+
+        //[Theory]
+        public void MultiplyTest<T>(T leftOperand, T rightOperand, T expectedResult)
+        {
+
+        }
+
+        //[Theory]
+        public void DivideTest<T>(T leftOperand, T rightOperand, T expectedResult)
+        {
+
+        }
+
+        //[Theory]
+        public void ModuloTest<T>(T leftOperand, T rightOperand, T expectedResult) 
+        {
+
+        }
+
+        //[Theory]
+        public void FactorialTest(int n, int expectedResult) 
+        {
+
+        }
+
+        //[Theory]
+        public void PowTest<T>(T b, int n, T expectedResult) 
+        {
+
+        }
+
+        //[Theory]
+        public void PowTest<T>(T x, T n, T expectedResult) 
         {
 
         }
