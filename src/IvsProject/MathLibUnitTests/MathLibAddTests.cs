02b451e7b576989d4660f2863ff4b3cd4063b77a
Christian Saloň <xsalon02@stud.fit.vutbr.cz>
2023-03-09 23:33:22 +0100

    Added tests for MathLib Add function for Floating-point types

--- src/IvsProject/MathLibUnitTests/MathLibAddTests.cs
+++ src/IvsProject/MathLibUnitTests/MathLibAddTests.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Math = MathLib.Math;
 
 namespace MathLibUnitTests
@@ -48,11 +49,67 @@ namespace MathLibUnitTests
         [InlineData(2, 0, 2)]
         [InlineData(0, 2, 2)]
         [InlineData(1, 2, 3)]
-        [InlineData(10000, 20000, 30000)]
         [InlineData(UInt32.MaxValue, 1, UInt32.MaxValue)]
         public void Add_UInt32Addends_ReturnsSum(UInt32 leftOperand, UInt32 rightOperand, UInt32 expectedResult)
         {
             UInt32 result = Math.Add(leftOperand, rightOperand);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(0.0, 0.0, 0.0)]
+        [InlineData(2.5, 0, 2.5)]
+        [InlineData(0, 2.1, 2.1)]
+        [InlineData(10000.2, 20000.4, 30000.6)]
+        [InlineData(-10000.2, -20000.2, -30000.0)]
+        [InlineData(10000.4, -20000.6, -10000.2)]
+        [InlineData(-10000.4, 20000.2, 9999.8)]
+        [InlineData(0.123456789, 0.987654321, 1.11111111)]
+        [InlineData(0.123456789, -0.987654321, -0.864197532)]
+        [InlineData(Single.MinValue, -1, Single.PositiveInfinity)]
+        [InlineData(Single.MaxValue, 1, Single.NegativeInfinity)]
+        public void Add_SingleAddends_ReturnsSum(Single leftOperand, Single rightOperand, Single expectedResult)
+        {
+            Single result = Math.Add(leftOperand, rightOperand);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(0.0, 0.0, 0.0)]
+        [InlineData(2.5, 0, 2.5)]
+        [InlineData(0, 2.1, 2.1)]
+        [InlineData(10000.2, 20000.4, 30000.6)]
+        [InlineData(-10000.2, -20000.2, -30000.0)]
+        [InlineData(10000.4, -20000.6, -10000.2)]
+        [InlineData(-10000.4, 20000.2, 9999.8)]
+        [InlineData(0.123456789, 0.987654321, 1.11111111)]
+        [InlineData(0.123456789, -0.987654321, -0.864197532)]
+        [InlineData(Double.MinValue, -1, Double.PositiveInfinity)]
+        [InlineData(Double.MaxValue, 1, Double.NegativeInfinity)]
+        public void Add_DoubleAddends_ReturnsSum(Double leftOperand, Double rightOperand, Double expectedResult)
+        {
+            Double result = Math.Add(leftOperand, rightOperand);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(0.0, 0.0, 0.0)]
+        [InlineData(2.5, 0, 2.5)]
+        [InlineData(0, 2.1, 2.1)]
+        [InlineData(10000.2, 20000.4, 30000.6)]
+        [InlineData(-10000.2, -20000.2, -30000.0)]
+        [InlineData(10000.4, -20000.6, -10000.2)]
+        [InlineData(-10000.4, 20000.2, 9999.8)]
+        [InlineData(0.123456789, 0.987654321, 1.11111111)]
+        [InlineData(0.123456789, -0.987654321, -0.864197532)]
+        [InlineData(-79228162514264337593543950335.0, -1.0, -79228162514264337593543950335.0)]
+        [InlineData(79228162514264337593543950335.0, 1.0, 79228162514264337593543950335.0)]
+        public void Add_DecimalAddends_ReturnsSum(Decimal leftOperand, Decimal rightOperand, Decimal expectedResult)
+        {
+            Decimal result = Math.Add(leftOperand, rightOperand);
 
             Assert.Equal(expectedResult, result);
         }
