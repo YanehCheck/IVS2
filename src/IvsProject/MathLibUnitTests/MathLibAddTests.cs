7febf2445c2cdc3616ab3011d61c41f165c6a3b9
Christian Saloň <xsalon02@stud.fit.vutbr.cz>
2023-03-10 00:05:27 +0100

    Code refactoring and adding comments

--- src/IvsProject/MathLibUnitTests/MathLibAddTests.cs
+++ src/IvsProject/MathLibUnitTests/MathLibAddTests.cs
@@ -1,28 +1,12 @@
-using System.Numerics;
 using Math = MathLib.Math;
 
 namespace MathLibUnitTests
 {
+    /// <summary>
+    /// Unit tests for MathLib Add function.
+    /// </summary>
     public class MathLibAddTests 
     {
-        /* Added simple test structure
-         * ------------------------------------------------------------------------
-         * Do not forget to test the exceptions!
-         * i.e. negative int in factorial, divide by zero (see documentation for the full list of possible exceptions),
-         * but try to find unspecified exceptions :)
-         * To match house style prototype for testing exceptions should for example look like this:
-         * public void DivideTest_ExpectDivideByZeroException<T>(T leftOperand, T rightOperand, T expectedResult)
-         *
-         * Do not forget to test multiple types!
-         * Not only integers - i.e.              [InlineData(5,4,3)]
-         * But also decimals - i.e.              [InlineData(5m,4m,3m)]
-         * and other types like double, float, and so on...
-         * Also do not forget about edge cases like NaN, Infinity and more.
-         * (see https://learn.microsoft.com/en-us/dotnet/standard/numerics)
-         * ------------------------------------------------------------------------
-         * Also do not forget to delete this :P
-         */
-
         [Theory]
         [InlineData(0, 0, 0)]
         [InlineData(2, 0, 2)]
