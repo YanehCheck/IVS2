e3a537394cff3110710e52a69924fa60c3080a35
Christian Saloň <xsalon02@stud.fit.vutbr.cz>
2023-03-12 22:45:39 +0100

    Edited tolerance for floating-point arithmetic

--- src/IvsProject/MathLibUnitTests/Constants.cs
+++ src/IvsProject/MathLibUnitTests/Constants.cs
@@ -7,7 +7,7 @@
     {
         public const Decimal DECIMAL_MAX_VALUE = 79_228_162_514_264_337_593_543_950_335m;
         public const Decimal DECIMAL_MIN_VALUE = -79_228_162_514_264_337_593_543_950_335m;
-        public const Single EPSILON = 0.0001f;
-        public const Int32 PRECISION = 4;
+        public const Single EPSILON = 0.1f;
+        public const Int32 PRECISION = 1;
     }
 }
