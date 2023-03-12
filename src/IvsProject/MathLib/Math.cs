e2897f97353ea835b145eda6d9eb478e6df69b62
Michael Janeček <michael.janecek.159@seznam.cz>
2023-03-12 22:29:19 +0100

    Implemented rest of math library

--- src/IvsProject/MathLib/Math.cs
+++ src/IvsProject/MathLib/Math.cs
@@ -13,9 +13,24 @@ namespace MathLib {
         /// <returns>
         /// The sum of <paramref name="leftOperand"></paramref> and <paramref name="rightOperand"></paramref>.
         /// </returns>
+        /// <exception cref="NotFiniteNumberException"> Thrown when <paramref name="rightOperand"></paramref> or <paramref name="leftOperand"></paramref> is not a finite number.</exception>
+        /// <exception cref="OverflowException"></exception> 
         public static T Add<T>(T leftOperand, T rightOperand) where T : INumber<T> 
         {
-            throw new NotImplementedException();
+            if(!IsFinite(new[] { leftOperand, rightOperand }))
+                throw new NotFiniteNumberException();
+
+            T result;
+            checked 
+            {
+                result = leftOperand + rightOperand;
+            }
+
+            if(!IsFinite(new[] { result })) 
+            {
+                throw new OverflowException();
+            }
+            return result;
         }
 
         /// <summary>
@@ -26,9 +41,24 @@ namespace MathLib {
         /// <returns>
         /// The difference after subtracting <paramref name="rightOperand"></paramref> from <paramref name="leftOperand"></paramref>.
         /// </returns>
+        /// <exception cref="NotFiniteNumberException"> Thrown when <paramref name="rightOperand"></paramref> or <paramref name="leftOperand"></paramref> is not a finite number.</exception>
+        /// <exception cref="OverflowException"></exception> 
         public static T Subtract<T>(T leftOperand, T rightOperand) where T : INumber<T> 
         {
-            throw new NotImplementedException();
+            if(!IsFinite(new[] { leftOperand, rightOperand }))
+                throw new NotFiniteNumberException();
+
+            T result;
+            checked 
+            {
+                result = leftOperand - rightOperand;
+            }
+
+            if(!IsFinite(new[] { result })) 
+            {
+                throw new OverflowException();
+            }
+            return result;
         }
 
         /// <summary>
@@ -39,9 +69,24 @@ namespace MathLib {
         /// <returns>
         /// The product after multiplying <paramref name="leftOperand"></paramref> with <paramref name="rightOperand"></paramref>.
         /// </returns>
+        /// <exception cref="NotFiniteNumberException"> Thrown when <paramref name="rightOperand"></paramref> or <paramref name="leftOperand"></paramref> is not a finite number.</exception>
+        /// <exception cref="OverflowException"></exception> Thrown even if the calculation would result in non-finite number.
         public static T Multiply<T>(T leftOperand, T rightOperand) where T : INumber<T> 
         {
-            throw new NotImplementedException();
+            if(!IsFinite(new[] { leftOperand, rightOperand }))
+                throw new NotFiniteNumberException();
+
+            T result;
+            checked 
+            {
+                result = leftOperand * rightOperand;
+            }
+
+            if(!IsFinite(new[] { result })) 
+            {
+                throw new OverflowException();
+            }
+            return result;
         }
 
         /// <summary>
@@ -53,9 +98,30 @@ namespace MathLib {
         /// The quotient after dividing <paramref name="leftOperand">leftOperand</paramref> by <paramref name="rightOperand">rightOperand</paramref>.
         /// </returns>
         /// <exception cref="DivideByZeroException"> Thrown when the <paramref name="rightOperand">rightOperand</paramref> is zero.</exception>
+        /// <exception cref="NotFiniteNumberException"> Thrown when <paramref name="rightOperand"></paramref> or <paramref name="leftOperand"></paramref> is not a finite number.</exception>
+        /// <exception cref="OverflowException"></exception> Thrown even if the calculation would result in non-finite number.
         public static T Divide<T>(T leftOperand, T rightOperand) where T : INumber<T> 
         {
-            throw new NotImplementedException();
+            if(!IsFinite(new[] { leftOperand, rightOperand })) 
+                throw new NotFiniteNumberException();
+
+            // default(T) is zero in any numeric type
+            if(rightOperand == default(T)) 
+            {
+                throw new DivideByZeroException();
+            }
+
+            T result;
+            checked 
+            {
+                result = leftOperand / rightOperand;
+            }
+
+            if (!IsFinite(new []{ result })) 
+            {
+                throw new OverflowException();
+            }
+            return result;
         }
 
         /// <summary>
@@ -66,13 +132,15 @@ namespace MathLib {
         /// <returns>
         /// The remainder after dividing <paramref name="leftOperand"></paramref> by <paramref name="rightOperand"></paramref>.
         /// </returns>
-        /// <exception cref="DivideByZeroException"> Thrown when the <paramref name="rightOperand"></paramref> is zero.</exception>
-        /// 
-        public static T Modulo<T>(T leftOperand, T rightOperand) where T : INumber<T> {
+        /// <exception cref="DivideByZeroException"> Thrown when <paramref name="rightOperand"></paramref> is zero.</exception>
+        /// <exception cref="NotFiniteNumberException"> Thrown when <paramref name="rightOperand"></paramref> or <paramref name="leftOperand"></paramref> is not a finite number.</exception>
+        public static T Modulo<T>(T leftOperand, T rightOperand) where T : INumber<T> 
+        {
             if (!IsFinite(new[] { leftOperand, rightOperand })) throw new NotFiniteNumberException();
 
             // default(T) is zero in any numeric type
-            if (rightOperand == default(T)) {
+            if (rightOperand == default(T)) 
+            {
                 throw new DivideByZeroException();
             }
 
@@ -85,7 +153,8 @@ namespace MathLib {
             var isLeftNegative = Comparer<T>.Default.Compare(leftOperand, default) < 0;
             var isRightNegative = Comparer<T>.Default.Compare(rightOperand, default) < 0;
 
-            if (isLeftNegative && !isRightNegative || !isLeftNegative && isRightNegative) {
+            if (isLeftNegative && !isRightNegative || !isLeftNegative && isRightNegative) 
+            {
                 return -result;
             }
             return result;
@@ -99,6 +168,7 @@ namespace MathLib {
         /// The factorial of <paramref name="n"></paramref>.
         /// </returns>
         /// <exception cref="ArgumentException"> Thrown when <paramref name="n"></paramref> is negative.</exception>
+        /// <exception cref="OverflowException"></exception> 
         public static int Factorial(int n) 
         {
             if (n < 0) throw new ArgumentException();
@@ -106,8 +176,10 @@ namespace MathLib {
 
 
             int result = n;
-            for (int i = n - 1; i > 0; i--) {
-                checked {
+            for (int i = n - 1; i > 0; i--) 
+            {
+                checked 
+                {
                     result *= i;
                 }
             }
@@ -124,9 +196,38 @@ namespace MathLib {
         /// <paramref name="n"></paramref>th power of <paramref name="b"></paramref>.
         /// </returns>
         /// <exception cref="ArgumentException"> Thrown when <paramref name="n"></paramref> is negative.</exception>
+        /// <exception cref="NotFiniteNumberException"> Thrown when <paramref name="b"></paramref> is not a finite number.</exception>
+        /// <exception cref="OverflowException"></exception>
         public static T Pow<T>(T b, int n) where T : INumber<T> 
         {
-            throw new NotImplementedException();
+            if(!IsFinite(new[] { b }))
+                throw new NotFiniteNumberException();
+
+            if (n < 0) 
+            {
+                throw new ArgumentException();
+            }
+            if (n == 0) {
+                return (T) Convert.ChangeType(1, typeof(T));
+            }
+            if (b == default(T)) 
+            {
+                return default;
+            }
+
+            T result = b;
+            for (int i = 1; i < n; i++) 
+            {
+                checked 
+                {
+                    result *= b;
+                }
+            }
+
+            if(!IsFinite(new[] { result })) {
+                throw new OverflowException();
+            }
+            return result;
         }
 
         /// <summary>
@@ -137,13 +238,36 @@ namespace MathLib {
         /// <returns>
         /// <paramref name="n"></paramref>th root of <paramref name="x"></paramref>.
         /// </returns>
-        /// <exception cref="ArgumentException"> Thrown when <paramref name="x"></paramref> is negative.</exception>
+        /// <exception cref="ArgumentException"> Thrown when the calculation would result in complex numbers.</exception>
+        /// <exception cref="DivideByZeroException"> Thrown when <paramref name="n"></paramref> is zero.</exception>
+        /// <exception cref="NotFiniteNumberException"></exception>
         public static T Root<T>(T x, T n) where T : INumber<T> 
         {
-            throw new NotImplementedException();
+            var isIndexNegative = Comparer<T>.Default.Compare(x, default) < 0;
+            var isRadicandNegative = Comparer<T>.Default.Compare(n, default) < 0;
+
+            if(!IsFinite(new[] { x, n }) || isIndexNegative && n == default)
+                throw new NotFiniteNumberException();
+            if(x == default)
+                throw new DivideByZeroException();
+
+            var xDouble = (double) Convert.ChangeType(x, typeof(double));
+            var nDouble = (double) Convert.ChangeType(n, typeof(double));
+            var result = System.Math.Pow(nDouble, 1 / xDouble);
+            var resultGeneric = (T) Convert.ChangeType(result, typeof(T));
+            if(((int) xDouble) % 2 == 0 && isRadicandNegative && ((int) result) == 1) {
+                throw new ArgumentException();
+            }
+            if(!IsFinite(new[] { resultGeneric })) 
+            {
+                throw new OverflowException();
+            }
+
+            return resultGeneric;
         }
 
-        private static bool IsFinite<T>(IEnumerable<T> numbers) where T : INumber<T> {
+        private static bool IsFinite<T>(IEnumerable<T> numbers) where T : INumber<T> 
+        {
             return !numbers.Any(number => 
                                 number.Equals(double.NegativeInfinity) || number.Equals(float.NegativeInfinity) ||
                                 number.Equals(double.PositiveInfinity) || number.Equals(float.PositiveInfinity) ||
