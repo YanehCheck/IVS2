e6418323c86270785f2d6ce7f8240336ac2fa719
xcechm13 <roharope@seznam.cz>
2023-04-23 17:18:41 +0200

    modified layout of zero button, added hover effect for buttons

--- src/IvsProject/CalculatorDesktopApp/Models/ThemeModel.cs
+++ src/IvsProject/CalculatorDesktopApp/Models/ThemeModel.cs
@@ -14,28 +14,40 @@ namespace CalculatorDesktopApp.Models
         public Color TextColor { get; set; }
         public Color ButtonNumbersColor { get; set; }
         public Color ButtonElseColor { get; set; }
+        public Color ButtonEqualColor { get; set; }
         public Color SettingsSectionColor { get; set; }
+        public Color ButtonElseHoverColor { get; set; }
+        public Color ButtonNumbersHoverColor { get; set; }
+        public Color ButtonEqualHoverColor { get; set; }
         public string QuestionMarkSource { get; set; }
         public string SettingsSource { get; set; }
 
         public static ThemeModel Dark => new()
         {
-            BackgroundColor = Color.FromRgb(27,33,35),
+            BackgroundColor = Color.FromRgb(27, 33, 35),
             TextColor = Color.FromRgb(255, 255, 255),
             ButtonNumbersColor = Color.FromRgb(59, 59, 59),
             ButtonElseColor = Color.FromRgb(50, 50, 50),
+            ButtonEqualColor = Color.FromRgb(161, 96, 251),
             SettingsSectionColor = Color.FromRgb(180, 180, 180),
+            ButtonNumbersHoverColor = Color.FromRgb(69, 69, 69),
+            ButtonElseHoverColor = Color.FromRgb(60, 60, 60),
+            ButtonEqualHoverColor = Color.FromRgb(150, 72, 245),
             QuestionMarkSource = "qm_white.png",
             SettingsSource = "settings_white.png"
         };
 
         public static ThemeModel Light => new()
         {
-            BackgroundColor = Color.FromRgb(235,235, 235),
-            TextColor = Color.FromRgb(0,0,0),
+            BackgroundColor = Color.FromRgb(235, 235, 235),
+            TextColor = Color.FromRgb(0, 0, 0),
             ButtonNumbersColor = Color.FromRgb(249, 249, 249),
             ButtonElseColor = Color.FromRgb(239, 239, 239),
+            ButtonEqualColor = Color.FromRgb(161, 96, 251),
             SettingsSectionColor = Color.FromRgb(40, 40, 40),
+            ButtonNumbersHoverColor = Color.FromRgb(239, 239, 239),
+            ButtonElseHoverColor = Color.FromRgb(249, 249, 249),
+            ButtonEqualHoverColor = Color.FromRgb(150, 72, 245),
             QuestionMarkSource = "qm_black.png",
             SettingsSource = "settings_black.png"
         };
